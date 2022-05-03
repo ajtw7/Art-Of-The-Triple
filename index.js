2a59bcf1587b5fb6861d574a0979e6713844ac6f
Andrew <ayjtw7@gmail.com>
2022-05-03 14:08:56 -0400

    added styles to nav links, added background, implemented XMLHttpRequest for YouTube V3 API

---
+++ index.js
@@ -0,0 +1,26 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+        var res = JSON.parse(xhttp.responseText)
+        var videoData = res.items.map(function (item) {
+            return item.snippet
+        });
+
+        //    initialize the content container
+        var contentContainer = document.querySelector('#video-divs');
+        videoData.forEach(function (video) {
+            console.log(video)
+            var videoDiv = document.createElement('div');
+            videoDiv.classList.add('videoDiv');
+            videoDiv.innerHTML = `
+                    <img src=${video.thumbnails.default.url} >
+                    <h6>${video.title}</h6>`;
+
+            contentContainer.appendChild(videoDiv);
+
+        });
+    }
+};
+xhttp.open("GET", "https://www.googleapis.com/youtube/v3/search?part=snippet&q=Triple+Jump&type=video&maxResults=4&key=AIzaSyDvzCwOEp-M31sFovCG9aeJhZgvFb27a8M", true);
+xhttp.send();
